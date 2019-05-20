a1f3e8cce28e0ffcf182dfde3ec4c14bb56b4325
Mitch Fish <mfishco@gmail.com>
2019-05-20 10:11:50 -0600

    Added input field in greeting component

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -2,4 +2,7 @@ import { LightningElement, track } from 'lwc';
 
 export default class HelloWebComponent extends LightningElement {
     @track greeting = 'TrailBlazer';
+    handleGreetingChange(event) {
+        this.greeting = event.target.value;
+    }
 }
