7dfc5b205c34babbe2d6bb7818b7fa9da78dbd57
Mitch Fish <mfishco@gmail.com>
2019-05-20 10:08:34 -0600

    adding data binding

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HelloWebComponent extends LightningElement {}
+export default class HelloWebComponent extends LightningElement {
+    @track greeting = 'TrailBlazer';
+}
