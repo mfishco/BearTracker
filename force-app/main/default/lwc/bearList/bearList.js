ac21f9f26daf7d5bb4c7e53e4d3a1cbf598552e4
Mitch Fish <mfishco@gmail.com>
2019-05-22 10:36:49 -0600

    Add bearMap component and update bearList

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,13 +1,20 @@
-import { NavigationMixin } from 'lightning/navigation';
-import { loadStyle } from 'lightning/platformResourceLoader';
+import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';import { loadStyle } from 'lightning/platformResourceLoader';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 import { LightningElement, track, wire } from 'lwc';
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearListNav extends NavigationMixin(LightningElement) {
 	@track searchTerm = '';
+	@track bears;
+	@wire(CurrentPageReference) pageRef;
 	@wire(searchBears, {searchTerm: '$searchTerm'})
-	bears;
+	loadBears(result) {
+		this.bears = result;
+		if (result.data) {
+			fireEvent(this.pageRef, 'bearListUpdate', result.data);
+		}
+	}
 	connectedCallback() {
 		loadStyle(this, ursusResources + '/style.css');
 	}
